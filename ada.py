a8917aa4deddd735c87c91ddd3a0b34d12ed0682
facuarrese <162734700+facuarrese@users.noreply.github.com>
2024-04-05 11:36:37 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,10 @@
 def ada():
-    first_name = "AdA"
-    last_name = "LoVeLAce"
+   
+first_name= "AdA"
+last_name= "LoVeAce"
+full_name= first_name + " " + last-name
+full_name= f"{first_name} {last_name}"
+print(full_name.lower())
+print (full_name.title())
+print (full_name.upper())
+print (f"\t{full_name.lower}")
