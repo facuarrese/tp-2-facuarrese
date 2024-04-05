e95e1d51653da0f5a0ab50088f7b072355f088a3
facuarrese <162734700+facuarrese@users.noreply.github.com>
2024-04-05 11:59:42 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,15 @@
 def change():
     expense = 23.75
     money = 100
+print("Ingresar gasto:")
+expense =float(input())
+print("Dinero recibido")
+money=int(input())
+vuelto=(money-expense)
+print()
+print("Vuelto")
+print()
+print("Pesos:")
+print(int(vuelto))
+print("Centavos:")
+print(int((vuelto-(int(vuelto)))*100))
